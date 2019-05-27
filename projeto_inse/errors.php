aeb0a71f2229791ba56ab7a17ea6ac1029f79442
Brenno Oliveira <breennoliveira@live.com>
2019-05-27 20:39:12 -0300

    Mudança na formatação de errors.php, linhas mais justas e texto em vermelho.

--- projeto_inse/errors.php
+++ projeto_inse/errors.php
@@ -1,7 +1,7 @@
 <?php  if (count($errors) > 0) : ?>
   <div class="error">
-  	<?php foreach ($errors as $error) : ?>
-  	  <p><?php echo $error ?></p>
-  	<?php endforeach ?>
+  	<p style="color: red"><?php foreach ($errors as $error) : ?>
+  	  <?php echo $error ?> </br>
+  	<?php endforeach ?></p>
   </div>
 <?php  endif ?>
