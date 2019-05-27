52d8c80338ba010e869fdd38eb4c0405e7be00c2
Brenno Oliveira <breennoliveira@live.com>
2019-05-27 20:27:30 -0300

    team.php revisado, erros de português corrigidos, adicionado minha descrição.

--- projeto_inse/includes/team.php
+++ projeto_inse/includes/team.php
@@ -2,14 +2,14 @@
 			<div class="wrapper style5">
 				<section id="team" class="container">
 					<header class="major">
-						<h2>A aquipe</h2>
+						<h2>A Equipe</h2>
 						<span class="byline">Descubra um pouco mais sobre a gente </span>
 					</header>
 					<div class="row">
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
-							<h3>Brenno Olivera</h3>
-							<p>In posuere eleifend odio quisque semper augue wisi ligula.</p>
+							<h3>Brenno Olivera Fernandes</h3>
+							<p>22 anos, trabalha com Mainframe na IBM, graduando em Sistemas de Informação</p>
 						</div>
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
@@ -24,7 +24,7 @@
 						<div class="3u">
 							<a href="#" class="image"><img src="images/placeholder.png" alt=""></a>
 							<h3>Orandi Falsarella</h3>
-							<p>Professor orientando, graudado, mestre e doutor.</p>
+							<p>Professor orientando, graduado, mestre e doutor.</p>
 						</div>
 					</div>
 				</section>
